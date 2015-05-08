b06b30b238678367bd7e5db3ad694dc00405310e
Christopher Strickland <mountaindust@gmail.com>
2015-05-08 12:05:40 -0400

    Adds comments, ndary daily rain, spatial mesh, p
    This commit adds many features, including the spatial mesh, p function
    (probability density), and some basic comments for new and existing
    functions. It also converts each day's rainfall from a list of ndarrays to
    a 2-D ndarray for convenence in later functions.

--- ParasitoidModel.py
+++ ParasitoidModel.py
@@ -11,9 +11,26 @@ from __future__ import division
 import numpy as np
 import scipy.linalg as linalg
 import scipy.stats as stats
+from scipy import fftpack
 import pymc as pm
 
 #we need to fix units for time. lets say t is in hours.
+
+#SPATIAL DOMAIN:
+# Release point will be placed in the center
+# Domain will be defined in terms of distance from release and resolution
+# FUTURE: assign cells UTM values when plotting
+rad_dist = 8000.0 #distance from release point to a side of the domain (meters)
+rad_res = 4000.0 #this will tranlate to a 2m^2 cell
+
+# Implementation detail:
+# Formation of each p(x,t_i), fft of each one, and ifft will need to be done in
+#   parallel when doing Bayesian inference. fft of 8000^2 is 5.6 sec.
+#   Want: number of processors = number of days to simulate
+dom_len = rad_res*2+1 #number of cells along one dimension of domain
+dom_ticks = np.linspace(-rad_dist,rad_dist,dom_len) #label the center of each cell
+                                                    #center cell is 0
+cell_dist = rad_dist/rad_res #dist from one cell to neighbor cell.
 
 # Reading the observed emergence data from a text file.
 def emergence_data(site_name):
@@ -75,19 +92,29 @@ def read_wind_file(site_name):
         else:
             wind_data[day] = [np.array([windx,windy,windr,theta])]
     wind_file.close()
+    
+    #convert each list of ndarrays to a single ndarray where rows are times,
+    #  columns are the windx,windy,windr,theta. This allows fancy slicing.
+    for day in wind_data:
+        wind_data[day] = np.array(wind_data[day])
 
     return wind_data
     #this returns a dictionary of days, with each day pointing to
-    #an ordered list of numpy arrays. Each member of the list is a time.
+    #a 2-D ndarray. Rows are times, columns are the windx,windy,windr,theta
 
 ##########    Model functions    ##########
 
 #Probability of flying under given wind conditions
 def g(windr, aw, bw):
+    # windr -- wind speed
+    # aw, bw -- logistic parameters (shape and bias)
     return 1.0 / (1. + np.exp(bw * (windr + aw)))
 
 #Probability of flying at n discrete times of the day, equally spaced
 def f(n, a1, b1, a2, b2):
+    # n -- number of wind data points per day available
+    # a1,b1,a2,b2 -- logistic parameters (shape and bias)
+
     #t is in hours, and denotes start time of flight.
     #(this is sort of weird, because it looks like wind was recorded starting
     #after the first 30 min)
@@ -101,12 +128,47 @@ def D(sig_x, sig_y, rho):
                      [rho*sig_x*sig_y, sig_y^2]])
     
 #Probability of flying per unit time under given conditions
-def h(day_wind, Pf, aw, bw, a1, b1, a2, b2):
-    #day_wind is a list of np.array wind directions
-    #day_wind[0] = np.array([windx,windy,windr,theta])
-    n = len(day_wind)
+def h(day_wind, lam, aw, bw, a1, b1, a2, b2):
+    # day_wind -- ndarray of wind directions
+    # lam -- constant
+    # aw,bw -- g function constants
+    # a1,b1,a2,b2 -- f function constants
+    #day_wind[0,:] = np.array([windx,windy,windr,theta])
+
+    n = day_wind.shape[0] #number of wind data entries in the day
     #get just the windr values
-    windary = np.array(day_wind)
-    windr = windary[:,2]
+    windr = day_wind[:,2]
     f_times_g = f(n,a1,b1,a2,b2)*g(windr,aw,bw)
-    return Pf*f_times_g/np.sum(f_times_g) #np.array of length len(day_wind)
+    return lam*f_times_g/np.sum(f_times_g) #np.array of length len(day_wind)
+    
+#Distance traveled through advection
+def mu(t_indx,day_wind,r):
+    # t_indx -- index in wind data
+    # day_wind -- ndarray of wind directions
+    # r -- constant
+    return r*day_wind[t_indx,0:2]
+    
+#Prob density for a given day, returned as an ndarray, shape given by a global
+def p(day,wind_data):
+    # day -- day since release
+    # wind_data -- dictionary of wind data
+    stdnormal = stats.multivariate_normal(np.array([0,0]),D(sig_x,sig_y,rho))
+    ppdf = np.zeros((dom_len,dom_len))
+    day_wind = wind_data[day]
+    hprob = h(day_wind, lam, aw, bw, a1, b1, a2, b2)
+    for t_indx in xrange(0,day_wind.shape[0]):
+        #calculate integral in an intelligent way.
+        #we know the distribution is centered around mu(t) at each t_indx
+        mu_vec = mu(t_indx,day_wind,r)
+        #translate into cell location
+        adv_cent = np.round(mu_vec/cell_dist)+np.array([rad_res,rad_res])
+        #now only worry about a normal distribution nearby this center
+        for ii in xrange(-40,40):
+            for jj in xrange(-40,40):
+                cellx = adv_cent[0]+ii
+                celly = adv_cent[1]+jj
+                #check boundaries (probably not necessary)
+                if 0<=cellx<dom_len and 0<=celly<dom_len:
+                    ppdf[cellx,celly] = ppdf[cellx,celly] + hprob[t_indx]\
+                    *stdnormal.pdf(np.array([ii*cell_dist,jj*cell_dist]))
+    return ppdf
