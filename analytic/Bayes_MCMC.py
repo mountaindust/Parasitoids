3854293a6e13269d12f63a114ef476b0500895ed
Christopher Strickland <mountaindust@gmail.com>
2016-04-18 11:52:00 -0400

    Adds test_Bayes, main() & bug fixes in Bayes_MCMC

--- analytic/Bayes_MCMC.py
+++ analytic/Bayes_MCMC.py
@@ -22,24 +22,7 @@ import pymc as pm
 import globalvars
 from Run import Params
 import ParasitoidModel as PM
-from CalcSol import get_solutions
-
-
-location = 'Kalbar'
-
-params = Params()
-# Set parameters specific to Bayesian runs
-params.PLOT = False
-params.OUTPUT = False
-params.ndays = -1 # locinfo.get_landscape_sample_datesPR()[-1]?
-
-# This sends a message to CalcSol on whether or not to use CUDA
-if params.CUDA:
-    globalvars.cuda = True
-else:
-    globalvars.cuda = False
-# get wind data and day labels
-wind_data,days = PM.get_wind_data(*params.get_wind_params())
+from CalcSol import get_populations
 
 
 
@@ -83,8 +66,7 @@ class LocInfo():
         #   the cell where the collection was centered.
         
         # First two columns are x,y locations from release in meters.
-        self.grid_cells = np.array(np.around(self.grid_data[:,0]/res),
-                                    np.around(self.grid_data[:,1]/res)).T
+        self.grid_cells = np.around(self.grid_data[:,0:2]/res)
         # Alias sampling effort in each grid cell
         self.grid_samples = self.grid_data[:,3]
         # Alias collection effort
@@ -159,18 +141,19 @@ def get_fields(filename,center):
         verts = []
         codes = []
         for line in f:
+            # deal with possible comments
+            c_ind = line.find('#')
+            if c_ind >= 0:
+                line = line[:c_ind]
             if line.strip() == '':
-                # end of current polygon. convert into Path object.
-                verts.append((0.,0.)) # ignored
-                codes.append(Path.CLOSEPOLY)
-                polys.append(Path(verts,codes))
-                verts = []
-                codes = []
+                if len(verts) > 0:
+                    # end of current polygon. convert into Path object.
+                    verts.append((0.,0.)) # ignored
+                    codes.append(Path.CLOSEPOLY)
+                    polys.append(Path(verts,codes))
+                    verts = []
+                    codes = []
             else:
-                # deal with possible comments
-                c_ind = line.find('#')
-                if c_ind >= 0:
-                    line = line[:c_ind]
                 # parse the data
                 vals = line.split(',')
                 verts.append(latlong_tocoord(
@@ -203,8 +186,8 @@ def get_field_cells(polys,domain_info):
     fields = []
     res = domain_info[0]/domain_info[1] #cell resolution
     # construct a list of all x,y coords (in meters) for the center of each cell
-    centers = [(x,y) for x in range(domain_info[1],-domain_info[1]-1,-1)*res
-                        for y in range(-domain_info[1],domain_info[1]+1)*res]
+    centers = [(x*res,y*res) for x in range(domain_info[1],-domain_info[1]-1,-1)
+                            for y in range(-domain_info[1],domain_info[1]+1)]
     for poly in polys:
         fields.append(np.argwhere(
             poly.contains_points(centers).reshape(
@@ -241,99 +224,6 @@ def get_release_grid(filename):
     return grid_data
             
     
-
-###############################################################################
-#                                                                             #
-#                              PYMC Setup                                     #
-#                                                                             #
-###############################################################################
-locinfo = LocInfo(location,params.domain_info)
-    
-    
-    
-@pm.deterministic
-def params_obj(params=params,g_aw=g_aw,g_bw=g_bw,f_a1=f_a1,f_b1=f_b1,
-    f_a2=f_a2,f_b2=f_b2,sig_x=sig_x,sig_y=sig_y,corr=corr,lam=lam,mu_r=mu_r):
-    '''Return altered parameter object to be passed in to simulation'''
-    
-    # g wind function parameters
-    params.g_params = (g_aw,g_bw)
-    # f time of day function parameters
-    params.f_params = (f_a1,f_b1,f_a2,f_b2)
-    # Diffusion coefficients
-    params.Dparams = (sig_x,sig_y,corr)
-    # Probability of any flight during the day under ideal circumstances
-    params.lam = lam
-    
-    # TRY BOTH - VARYING mu_r OR n_periods
-    # scaling flight advection to wind advection
-    params.mu_r = mu_r
-    # number of time periods (based on interp_num) in one flight
-    #params.n_periods = n_periods # if interp_num = 30, this is # of minutes
-    
-    return params
-    
-    
-    
-@pm.deterministic
-def run_model(params=params_obj,wind_data=wind_data,days=days):
-    '''This function acts as an interface between PyMC and the model.
-    It provides the model based on stochastic parameters in params.
-    '''
-    
-    ### PHASE ONE ###
-    # First, get spread probability for each day as a coo sparse matrix
-    pmf_list = []
-    max_shape = np.array([0,0])
-    print("Calculating each day's spread in parallel...")
-    pm_args = [(days[0],wind_data,*params.get_model_params(),
-            params.r_start)]
-    pm_args.extend([(day,wind_data,*params.get_model_params()) 
-            for day in days[1:params.ndays]])
-    pool = Pool()
-    pmf_list = pool.starmap(PM.prob_mass,pm_args)
-    pool.close()
-    pool.join()
-    for pmf in pmf_list:
-        for dim in range(2):
-            if pmf.shape[dim] > max_shape[dim]:
-                max_shape[dim] = pmf.shape[dim]
-                
-    r_spread = [] # holds the one-day spread for each release day.
-    
-    
-    # Reshape the prob. mass function of each release day into solution form
-    for ii in range(params.r_dur):
-        offset = params.domain_info[1] - pmf_list[ii].shape[0]//2
-        dom_len = params.domain_info[1]*2 + 1
-        r_spread.append(sparse.coo_matrix((pmf_list[ii].data, 
-            (pmf_list[ii].row+offset,pmf_list[ii].col+offset)),
-            shape=(dom_len,dom_len)).tocsr())
-    
-    ### PHASE TWO ###
-    # Pass the probability list, pmf_list, and other info to convolution solver.
-    #   This will return the finished population model.
-    with Capturing() as output:
-        modelsol = get_populations(r_spread,pmf_list,days,params.ndays,dom_len,
-                    max_shape,params.r_dur,params.r_number,params.r_mthd())
-    
-    # modelsol now holds the model results for this run
-    return modelsol
-    
-
-
-@pm.deterministic
-def landscape_emergence(modelsol=run_model,beta=beta):
-    '''Emergence observed from the model. This parses modelsol using the
-    sampling model. It should return a 2D array where the rows are each day
-    sampled and the columns are the number of E. Hayati observed.
-    
-    Args:
-        modelsol: model solution
-        beta: Bernoulli trial probability by density'''
-    pass
-    
-    
     
 class Capturing(list):
     '''This class creates a list object that can be used in 'with' environments
@@ -355,8 +245,131 @@ class Capturing(list):
     def __exit__(self, *args):
         self.extend(self._stringio.getvalue().splitlines())
         sys.stdout = self._stdout
+    
+    
 
+###############################################################################
+#                                                                             #
+#                              PYMC Setup                                     #
+#                                                                             #
+###############################################################################
+
+def main():
+
+    params = Params()
+    # Set up location here with command line arguments in a list.
+    params.cmd_line_chg(['--kalbar'])
+    assert params.site_name+'fields.txt' == 'data/kalbarfields.txt'
+    # Set parameters specific to Bayesian runs
+    params.PLOT = False
+    params.OUTPUT = False
+    params.ndays = -1 # locinfo.get_landscape_sample_datesPR()[-1]?
+
+    # This sends a message to CalcSol on whether or not to use CUDA
+    if params.CUDA:
+        globalvars.cuda = True
+    else:
+        globalvars.cuda = False
+    # get wind data and day labels
+    wind_data,days = PM.get_wind_data(*params.get_wind_params())
+
+    locinfo = LocInfo(params.site_name,params.coord,params.domain_info)
+        
+    lam = pm.Beta("lambda",5,1)
+    f_a1 = pm.TruncatedNormal("a_1",6,1,0,12)
+    f_a2 = pm.TruncatedNormal("a_2",18,1,12,24)
+    f_b1 = pm.Gamma("b_1",3,1)
+    f_b2 = pm.Gamma("b_2",3,1)
+    g_aw = pm.Gamma("a_w",2.2,1)
+    g_bw = pm.Gamma("b_w",5,1)
+    sig_x = pm.Gamma("sig_x",6,1)
+    sig_y = pm.Gamma("sig_y",6,1)
+    corr = pm.Uniform("rho",-1,1)
+    mu_r = pm.Normal("mu_r",1.5,1/0.75**2)
+    #n_periods = pm.Poisson("t_dur",10)
+        
+    @pm.deterministic
+    def params_obj(params=params,g_aw=g_aw,g_bw=g_bw,f_a1=f_a1,f_b1=f_b1,
+        f_a2=f_a2,f_b2=f_b2,sig_x=sig_x,sig_y=sig_y,corr=corr,lam=lam,mu_r=mu_r):
+        '''Return altered parameter object to be passed in to simulation'''
+        
+        # g wind function parameters
+        params.g_params = (g_aw,g_bw)
+        # f time of day function parameters
+        params.f_params = (f_a1,f_b1,f_a2,f_b2)
+        # Diffusion coefficients
+        params.Dparams = (sig_x,sig_y,corr)
+        # Probability of any flight during the day under ideal circumstances
+        params.lam = lam
+        
+        # TRY BOTH - VARYING mu_r OR n_periods
+        # scaling flight advection to wind advection
+        params.mu_r = mu_r
+        # number of time periods (based on interp_num) in one flight
+        #params.n_periods = n_periods # if interp_num = 30, this is # of minutes
+        
+        return params
+        
+        
+        
+    @pm.deterministic
+    def run_model(params=params_obj,wind_data=wind_data,days=days):
+        '''This function acts as an interface between PyMC and the model.
+        It provides the model based on stochastic parameters in params.
+        '''
+        
+        ### PHASE ONE ###
+        # First, get spread probability for each day as a coo sparse matrix
+        pmf_list = []
+        max_shape = np.array([0,0])
+        print("Calculating each day's spread in parallel...")
+        pm_args = [(days[0],wind_data,*params.get_model_params(),
+                params.r_start)]
+        pm_args.extend([(day,wind_data,*params.get_model_params()) 
+                for day in days[1:params.ndays]])
+        pool = Pool()
+        pmf_list = pool.starmap(PM.prob_mass,pm_args)
+        pool.close()
+        pool.join()
+        for pmf in pmf_list:
+            for dim in range(2):
+                if pmf.shape[dim] > max_shape[dim]:
+                    max_shape[dim] = pmf.shape[dim]
+                    
+        r_spread = [] # holds the one-day spread for each release day.
+        
+        
+        # Reshape the prob. mass function of each release day into solution form
+        for ii in range(params.r_dur):
+            offset = params.domain_info[1] - pmf_list[ii].shape[0]//2
+            dom_len = params.domain_info[1]*2 + 1
+            r_spread.append(sparse.coo_matrix((pmf_list[ii].data, 
+                (pmf_list[ii].row+offset,pmf_list[ii].col+offset)),
+                shape=(dom_len,dom_len)).tocsr())
+        
+        ### PHASE TWO ###
+        # Pass the probability list, pmf_list, and other info to convolution solver.
+        #   This will return the finished population model.
+        with Capturing() as output:
+            modelsol = get_populations(r_spread,pmf_list,days,params.ndays,dom_len,
+                        max_shape,params.r_dur,params.r_number,params.r_mthd())
+        
+        # modelsol now holds the model results for this run
+        return modelsol
+        
+
+
+    # @pm.deterministic
+    # def landscape_emergence(modelsol=run_model,beta=beta):
+        # '''Emergence observed from the model. This parses modelsol using the
+        # sampling model. It should return a 2D array where the rows are each day
+        # sampled and the columns are the number of E. Hayati observed.
+        
+        # Args:
+            # modelsol: model solution
+            # beta: Bernoulli trial probability by density'''
+        # pass
 
     
-# if __name__ == "__main__":
-    # main(sys.argv[1:])
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ analytic/tests/test_Bayes.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python3
+
+'''Test module for Bayes_MCMC
+
+Author: Christopher Strickland'''
+
+import pytest
+import numpy as np
+from matplotlib.path import Path
+import Bayes_MCMC as Bayes
+
+###############################################################################
+#                                                                             #
+#                              Test Fixtures                                  #
+#                                                                             #
+###############################################################################
+
+@pytest.fixture()
+def field_info():
+   # kalbar info
+   filename = 'data/kalbar'
+   center = (-27.945752,152.58474)
+   return (filename,center)
+   
+   
+   
+###############################################################################
+#                                                                             #
+#                                   Tests                                     #
+#                                                                             #
+###############################################################################
+
+def test_get_fields(field_info):
+    filename,center = field_info
+    filename += 'fields.txt'
+    polys = Bayes.get_fields(filename,center)
+    # polys should be a list of Path objects
+    assert type(polys) is list
+    assert type(polys[0]) is Path
+    assert len(polys) == 7
