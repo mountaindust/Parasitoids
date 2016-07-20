0b9ce40811ee05b5ba4601ab3f8c3daf68c86e2a
Christopher Strickland <mountaindust@gmail.com>
2016-07-20 11:01:09 -0400

    Increase std prior by factor of 10 to pub numbers

--- analytic/Bayes_Run.py
+++ analytic/Bayes_Run.py
@@ -102,15 +102,15 @@ def main():
     g_aw = pm.Gamma("a_w",2.2,1,value=2.2)
     g_bw = pm.Gamma("b_w",5,1,value=5)
     # flight diffusion parameters. note: mean is average over flight advection
-    sig_x = pm.Gamma("sig_x",32.4,1,value=32.4) 
-    sig_y = pm.Gamma("sig_y",16.2,1,value=16.2) 
+    sig_x = pm.Gamma("sig_x",211,1,value=211) #32.4
+    sig_y = pm.Gamma("sig_y",106,1,value=106) #16.2
     corr_p = pm.Beta("rho_p",5,5,value=0.5,trace=False,plot=False)
     @pm.deterministic(trace=True,plot=True)
     def corr(corr_p=corr_p):
         return corr_p*2 - 1    
     # local spread paramters
-    sig_x_l = pm.Gamma("sig_xl",32.4,1,value=32.4)
-    sig_y_l = pm.Gamma("sig_yl",16.2,1,value=16.2)
+    sig_x_l = pm.Gamma("sig_xl",211,1,value=211) #32.4
+    sig_y_l = pm.Gamma("sig_yl",106,1,value=106) #16.2
     corr_l_p = pm.Beta("rho_l_p",5,5,value=0.5,trace=False,plot=False)
     @pm.deterministic(trace=True,plot=True)
     def corr_l(corr_l_p=corr_l_p):
